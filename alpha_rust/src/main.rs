8a0ad6251ce56b6b7aa4020492121bc42cea7b58
Advaith Anand <aadi@Advaiths-MacBook-Pro.local>
2020-08-07 22:19:57 -0700

    Working print statement and variable assignment with strings only

--- alpha_rust/src/main.rs
+++ alpha_rust/src/main.rs
@@ -1,14 +1,13 @@
 use std::env;
 use std::fs;
 use std::time::Instant;
+use std::collections::HashMap;
+mod tokenize;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let num_args = args.len();
-    let mut mode = "none";
-    let mut escaped = false;
-    let mut com_num = 1;
 
     if num_args != 2 {
         panic!("Please provide only a single text file.");
@@ -18,83 +17,20 @@ fn main() {
         .expect("Failed to read file.");
 
     let start = Instant::now();
-    let mut cur_word = String::new();
-    let mut strings: Vec<String> = Vec::new();
-    let mut command_types: Vec<String> = Vec::new();
-    let mut command_words = String::new();
-    let mut new_command = true;
+    
+    let tokenized = tokenize::tokenize(&contents);
+    let mut variables = HashMap::new();
 
-    for c in contents.chars() {
-        match c {
-            '"' => {
-                if escaped {
-                    escaped = false;
-                    cur_word.push('"');
-                    display(&mode, c, com_num);
-                } else if let "quote" = mode {
-                    mode = "none";
-                    strings.push(cur_word);
-                    cur_word = String::new()
-                } else {
-                    mode = "quote";
-                }
-            },
-            ' ' => {
-                if escaped {
-                    escaped = false;
-                } else if let "quote" = mode {
-                    cur_word.push(c);
-                    display(&mode, c, com_num);
-                } else {
-                    new_command = false;
-                    display(&mode, c, com_num);
-                }
-            },
-            '.' => {
-                if escaped {
-                    escaped = false;
-                    cur_word.push('.');
-                    display(&mode, c, com_num);
-                } else {
-                    com_num += 1;
-                    command_types.push(cur_command_type);
-                    new_command = true;
-                    escaped = true;
-                    cur_command_type = String::new()
-                }
-            },
-            '\\' => {
-                if escaped {
-                    escaped = false;
-                    cur_word.push('\\');
-                    display(&mode, c, com_num);
-                } else {
-                    escaped = true
-                }
-            },
-            _ => {
-                if escaped {
-                    escaped = false
-                } else {
-                    if let "quote" = mode {
-                        cur_word.push(c);
-                    } else if new_command {
-                        cur_command_type.push(c);
-                    }
-                    display(&mode, c, com_num);
-                }
-            }
+    for i in tokenized.iter() {
+        if i[0] == "Print" {
+            println!("{}", i[1]);
+        } else if i[0] == "Assign" {
+            variables.insert(&i[3], &i[1]);
         }
     }
 
-    println!("{:?}", strings);
-    println!("{}", contents);
-    println!("{:?}", command_types);
+    println!("Variables: {:?}", variables);
 
     let duration = start.elapsed();
     println!("Time: {:?}", duration);
-}
-
-fn display(mode: &str, c: char, num: u32) {
-    println!("{}: \"{}\" mode >> {}", num, mode, c);
 }
---
+++ alpha_rust/src/tokenize.rs
@@ -0,0 +1,64 @@
+pub fn tokenize(contents: &String) -> Vec<Vec<String>> {
+    let mut tokenized: Vec<Vec<String>> = Vec::new();
+    let mut command: Vec<String> = Vec::new();
+    let mut token = String::new();
+    let mut mode = "none";
+    let mut escaped = false;
+
+    for c in contents.chars() {
+        match c {
+            '"' => {
+                if escaped {
+                    escaped = false;
+                    token.push('"');
+                } else if let "quote" = mode {
+                    mode = "none";
+                    command.push(token);
+                    token = String::new()
+                } else {
+                    mode = "quote";
+                }
+            },
+            ' ' => {
+                if escaped {
+                    escaped = false;
+                } else if let "quote" = mode {
+                    token.push(c);
+                } else if token != "" {
+                    command.push(token);
+                    token = String::new();
+                }
+            },
+            '.' => {
+                if escaped {
+                    escaped = false;
+                    token.push('.');
+                } else {
+                    if token != "" {
+                        command.push(token);
+                        token = String::new();
+                    }
+                    tokenized.push(command);
+                    command = Vec::new();
+                }
+            },
+            '\\' => {
+                if escaped {
+                    escaped = false;
+                    token.push('\\');
+                } else {
+                    escaped = true
+                }
+            },
+            _ => {
+                if escaped {
+                    escaped = false
+                } else {
+                    token.push(c);
+                }
+            }
+        }
+    }
+
+    tokenized
+}
